350aaef444ae6ffd3ed5034e10ffcef22087495d
Timcanpy <view619@gmail.com>
2018-10-27 21:12:53 -0400

    Eliminated DLC check bypass.

--- QoL Mods/Overrides.cs
+++ QoL Mods/Overrides.cs
@@ -9,34 +9,9 @@ namespace QoL_Mods
     [FieldAccess(Class = "DLC.DLCChecker", Field = "IsOwner", Group = "WrestlerSearch")]
     [FieldAccess(Class = "DLC.DLCChecker", Field = "IsDownloaded", Group = "WrestlerSearch")]
     [FieldAccess(Class = "DLC.DLCChecker", Field = "IsInstalled", Group = "WrestlerSearch")]
-    
+
     class Overrides
     {
-        [Hook(TargetClass = "DLC.DLCChecker", TargetMethod = "IsValidDLCID", InjectionLocation = 0, InjectDirection = HookInjectDirection.Before, InjectFlags = HookInjectFlags.ModifyReturn, Group = "WrestlerSearch")]
-        public static bool ValidDLCID(out bool result)
-        {
-            result = true;
-            return result;
-        }
-        [Hook(TargetClass = "DLC.DLCChecker", TargetMethod = "IsOwner", InjectionLocation = 0, InjectDirection = HookInjectDirection.Before, InjectFlags = HookInjectFlags.ModifyReturn, Group = "WrestlerSearch")]
-        public static bool ValidOwner(out bool result)
-        {
-            result = true;
-            return result;
-        }
-        [Hook(TargetClass = "DLC.DLCChecker", TargetMethod = "IsDownloaded", InjectionLocation = 0, InjectDirection = HookInjectDirection.Before, InjectFlags = HookInjectFlags.ModifyReturn, Group = "WrestlerSearch")]
-        public static bool ValidDownload(out bool result)
-        {
-            result = true;
-            return result;
-        }
-        [Hook(TargetClass = "DLC.DLCChecker", TargetMethod = "IsInstalled", InjectionLocation = 0, InjectDirection = HookInjectDirection.Before, InjectFlags = HookInjectFlags.ModifyReturn, Group = "WrestlerSearch")]
-        public static bool ValidInstall(out bool result)
-        {
-            result = true;
-            return result;
-        }
-
         [ControlPanel(Group = "WrestlerSearch")]
         public static Form MSForm()
         {
@@ -48,5 +23,5 @@ namespace QoL_Mods
                 return null;
             }
         }
-    }   
+    }
 }
