4ea71bafd8dc6fda7d2175883db676823313715a
Timcanpy <view619@gmail.com>
2018-11-08 22:53:16 -0500

    Added Ref Pinfall Positioning component. Included definitions for the mod features.

--- QoL Mods/Overrides.cs
+++ QoL Mods/Overrides.cs
@@ -5,10 +5,15 @@ using DLC;
 
 namespace QoL_Mods
 {
-    [FieldAccess(Class = "DLC.DLCChecker", Field = "IsValidDLCID", Group = "WrestlerSearch")]
-    [FieldAccess(Class = "DLC.DLCChecker", Field = "IsOwner", Group = "WrestlerSearch")]
-    [FieldAccess(Class = "DLC.DLCChecker", Field = "IsDownloaded", Group = "WrestlerSearch")]
-    [FieldAccess(Class = "DLC.DLCChecker", Field = "IsInstalled", Group = "WrestlerSearch")]
+    [GroupDescription(Group = "Wrestler Search", Name = "Wrestler Search Tool", Description = "Provides a UI for loading edits within Edit Mode.")]
+    [GroupDescription(Group = "Ref Positions For Pinfall", Name = "Referee Behavior Override", Description = "Forces the referee to move towards the active players after big moves performed late in a match. When the referee decides to start moving depends on his Involvement skill.")]
+    [FieldAccess(Class = "MatchMain", Field = "InitMatch", Group = "Wrestler Search")]
+
+    #region Pinfall Field Access
+    [FieldAccess(Class = "MatchEvaluation", Field = "EvaluateSkill", Group = "Ref Positions For Pinfall")]
+    [FieldAccess(Class = "Player", Field = "UpdatePlayer", Group = "Ref Positions For Pinfall")]
+    [FieldAccess(Class = "Referee", Field = "GoToPlayer", Group = "Ref Positions For Pinfall")]
+    #endregion
 
     class Overrides
     {
@@ -23,5 +28,75 @@ namespace QoL_Mods
                 return null;
             }
         }
+
+        #region Force Preemptive Pinfall Count
+
+        private static int downedPlayer;
+        [Hook(TargetClass = "MatchEvaluation", TargetMethod = "EvaluateSkill", InjectionLocation = int.MaxValue, InjectDirection = HookInjectDirection.Before, InjectFlags = HookInjectFlags.PassParametersVal, Group = "Ref Positions For Pinfall")]
+        public static void PrepareForPinfall(int plIDx, SkillData sd, SkillSlotAttr skillAttr)
+        {
+            if (MatchMain.inst.isMatchEnd)
+            {
+                return;
+            }
+            Player attacker = PlayerMan.inst.GetPlObj(plIDx);
+            Player defender = PlayerMan.inst.GetPlObj(attacker.TargetPlIdx);
+
+            //Prepare for pinfall
+            Referee mRef = RefereeMan.inst.GetRefereeObj();
+            int refInvolvement = mRef.RefePrm.interfereTime;
+            float multiplier = .25f;
+            switch (refInvolvement)
+            {
+                case 0:
+                    multiplier = .10f;
+                    break;
+                case 1:
+                    multiplier = .15f;
+                    break;
+                case 2:
+                    multiplier = .25f;
+                    break;
+                case 3:
+                    multiplier = .40f;
+                    break;
+                case 4:
+                    multiplier = .50f;
+                    break;
+                default:
+                    multiplier = .25f;
+                    break;
+            }
+            float checkValue = 65535 * multiplier;
+
+            if (((sd.primarySkillStrength == PrimarySkillStrength.H) && sd.anmLoopTimes == 0 && (defender.SP < checkValue || defender.HP < checkValue)) || defender.isCriticalMoveRecieved && attacker.Zone == ZoneEnum.InRing)
+            {
+                downedPlayer = defender.PlIdx;
+            }
+        }
+
+        [Hook(TargetClass = "Player", TargetMethod = "UpdatePlayer", InjectionLocation = 0, InjectDirection = HookInjectDirection.Before, InjectFlags = HookInjectFlags.PassInvokingInstance, Group = "Ref Positions For Pinfall")]
+        public static void CheckForPinfall(Player player)
+        {
+            if (MatchMain.inst.isMatchEnd)
+            {
+                return;
+            }
+
+            if (player.PlIdx == downedPlayer && (player.State == PlStateEnum.Down_FaceDown || player.State == PlStateEnum.Down_FaceUp) && player.Zone == ZoneEnum.InRing)
+            {
+                downedPlayer = -1;
+                CheckForFall(player.PlIdx);
+            }
+        }
+
+        public static void CheckForFall(int defender)
+        {
+            Referee mRef = RefereeMan.inst.GetRefereeObj();
+            mRef.GoToPlayer(defender, 0f);
+            mRef.RefeCount = 0;
+            mRef.NextState = RefeStateEnum.CheckSubmission;
+        }
+        #endregion
     }
 }
