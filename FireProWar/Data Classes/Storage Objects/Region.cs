15824b9a0a5c12e68b136d1c85ac6b3c4952588b
Timcanpy <view619@gmail.com>
2021-01-03 09:46:53 -0500

    Additional storage objects for AWS project

--- FireProWar/Data Classes/Storage Objects/Region.cs
+++ FireProWar/Data Classes/Storage Objects/Region.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Amazon.DynamoDBv2.DataModel;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,20 +7,32 @@ using System.Text;
 namespace FireProWar.Data_Classes.Storage_Objects
 {
     [Serializable]
+    [DynamoDBTable("Region")]
     class Region
     {
         #region Parameters
+        [DynamoDBHashKey]
         private int id;
 
         public int ID
         {
             get { return id; }
         }
+
+        [DynamoDBProperty]
         private String name;
 
         public String Name
         {
             get { return name; }
+        }
+        #endregion
+
+        #region Methods
+        public Region (int RegionID, string RegionName)
+        {
+            id = RegionID;
+            name = RegionName;
         }
         #endregion
     }
